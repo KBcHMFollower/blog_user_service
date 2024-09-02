53c4b08ced25a8fed9407861c9300a6af521b013
streliziy <vladislav13022004@gmail.com>
2024-09-02 17:31:05 +0300

    refactor: change deleted-posts message

--- internal/clients/amqpclient/messages/posts_deleted.go
+++ internal/clients/amqpclient/messages/posts_deleted.go
@@ -2,6 +2,11 @@ package messages
 
 import "github.com/google/uuid"
 
+const (
+	Success = "success"
+	Failed  = "failed"
+)
+
 type PostsDeleted struct {
 	EventId uuid.UUID `json:"event_id"`
 	Status  string    `json:"status"`
