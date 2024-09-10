a8b6835243a61ccb778fe19aed08d81763227da8
streliziy <vladislav13022004@gmail.com>
2024-09-10 14:13:03 +0300

    feat: make circuid breaker

--- internal/lib/circuid_breaker/breaker_state.go
+++ internal/lib/circuid_breaker/breaker_state.go
@@ -1,7 +1,12 @@
 package circuid_breaker
 
+import (
+	"github.com/benbjohnson/clock"
+	"time"
+)
+
 type breakerState interface {
-	State() breakerStateName
+	State() BreakerStateName
 	ready() bool
 	onEntry(cb *CircuitBreaker)
 	onSuccess(cb *CircuitBreaker)
@@ -9,29 +14,93 @@ type breakerState interface {
 	onExit(cb *CircuitBreaker)
 }
 
-type closedBreakerState struct{}
+type closedBreakerState struct {
+	ticker *clock.Ticker
+}
 
-func (cbs *closedBreakerState) State() breakerStateName      { return breakerClose }
-func (cbs *closedBreakerState) ready() bool                  { return true }
-func (cbs *closedBreakerState) onEntry(cb *CircuitBreaker)   { cb.counter.reset() }
-func (cbs *closedBreakerState) onSuccess(cb *CircuitBreaker) { cb.counter.incSuccess() }
-func (cbs *closedBreakerState) onFailure(cb *CircuitBreaker) { cb.counter.incFailure() }
-func (cbs *closedBreakerState) onExit(cb *CircuitBreaker)    {}
+func (cbs *closedBreakerState) State() BreakerStateName { return breakerClose }
+func (cbs *closedBreakerState) ready() bool             { return true }
+func (cbs *closedBreakerState) onEntry(cb *CircuitBreaker) {
+	cb.counter.reset()
+	if cb.openConditions.timeInterval == time.Duration(0) {
+		return
+	}
 
-type openBreakerState struct{}
+	cbs.ticker = cb.clock.Ticker(cb.openConditions.timeInterval)
+	go func() {
+		for {
+			select {
+			case <-cbs.ticker.C:
+				cb.checkOpen()
+			default:
+				cbs.ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+func (cbs *closedBreakerState) onSuccess(cb *CircuitBreaker) {}
+func (cbs *closedBreakerState) onFailure(cb *CircuitBreaker) {
+	if cb.checkConsecutiveFailuresCondition() {
+		cb.SetState(&openBreakerState{})
+	}
+}
+func (cbs *closedBreakerState) onExit(cb *CircuitBreaker) {
+	if cbs.ticker != nil {
+		cbs.ticker.Stop()
+	}
+}
 
-func (obs *openBreakerState) State() breakerStateName      { return breakerOpen }
-func (obs *openBreakerState) onEntry(cb *CircuitBreaker)   { cb.counter.reset() }
+type openBreakerState struct {
+	timer *clock.Timer
+}
+
+func (obs *openBreakerState) State() BreakerStateName { return breakerOpen }
+func (obs *openBreakerState) onEntry(cb *CircuitBreaker) {
+	cb.counter.reset()
+
+	obs.timer = cb.clock.AfterFunc(cb.openTime, func() {
+		cb.SetState(&halfOpenBreakerState{})
+	})
+}
 func (obs *openBreakerState) onSuccess(cb *CircuitBreaker) {}
 func (obs *openBreakerState) onFailure(cb *CircuitBreaker) {}
-func (obs *openBreakerState) onExit(cb *CircuitBreaker)    {}
-func (obs *openBreakerState) ready() bool                  { return false }
+func (obs *openBreakerState) onExit(cb *CircuitBreaker) {
+	if obs.timer != nil {
+		obs.timer.Stop()
+	}
+}
+func (obs *openBreakerState) ready() bool { return false }
 
-type halfOpenBreakerState struct{}
+type halfOpenBreakerState struct {
+	timer *clock.Timer
+}
 
-func (hobs *halfOpenBreakerState) State() breakerStateName      { return breakerHalfOpen }
-func (hobs *halfOpenBreakerState) onEntry(cb *CircuitBreaker)   { cb.counter.reset() }
-func (hobs *halfOpenBreakerState) onSuccess(cb *CircuitBreaker) { cb.counter.incSuccess() }
-func (hobs *halfOpenBreakerState) onFailure(cb *CircuitBreaker) { cb.counter.incFailure() }
-func (hobs *halfOpenBreakerState) onExit(cb *CircuitBreaker)    { cb.counter.incFailure() }
-func (hobs *halfOpenBreakerState) ready() bool                  { return true }
+func (hobs *halfOpenBreakerState) State() BreakerStateName { return breakerHalfOpen }
+func (hobs *halfOpenBreakerState) onEntry(cb *CircuitBreaker) {
+	cb.counter.reset()
+	if cb.closeConditions.duration == time.Duration(0) {
+		return
+	}
+
+	hobs.timer = cb.clock.AfterFunc(cb.closeConditions.duration, func() {
+		if cb.checkClose() {
+			cb.SetState(&closedBreakerState{})
+			return
+		}
+
+		cb.SetState(&halfOpenBreakerState{})
+	})
+}
+func (hobs *halfOpenBreakerState) onSuccess(cb *CircuitBreaker) {
+	if cb.checkConsecutiveSuccessCondition() {
+		cb.SetState(&closedBreakerState{})
+	}
+}
+func (hobs *halfOpenBreakerState) onFailure(cb *CircuitBreaker) {}
+func (hobs *halfOpenBreakerState) onExit(cb *CircuitBreaker) {
+	if hobs.timer != nil {
+		hobs.timer.Stop()
+	}
+}
+func (hobs *halfOpenBreakerState) ready() bool { return true }
--- internal/lib/circuid_breaker/circuit_breaker.go
+++ internal/lib/circuid_breaker/circuit_breaker.go
@@ -3,19 +3,22 @@ package circuid_breaker
 import (
 	"context"
 	"errors"
+	"github.com/KBcHMFollower/blog_user_service/internal/lib"
+	"github.com/benbjohnson/clock"
 	"sync"
+	"time"
 )
 
-type breakerStateName string
+type BreakerStateName string
 
 var (
 	ErrBreakerOpen = errors.New("breaker is open")
 )
 
 const (
-	breakerOpen     breakerStateName = "open"
-	breakerClose    breakerStateName = "close"
-	breakerHalfOpen breakerStateName = "half-open"
+	breakerOpen     BreakerStateName = "open"
+	breakerClose    BreakerStateName = "close"
+	breakerHalfOpen BreakerStateName = "half-open"
 )
 
 type Counter struct {
@@ -51,36 +54,119 @@ func (c *Counter) resetSuccess() {
 	c.consecutiveSuccess = 0
 }
 
+type CloseCondition struct {
+	duration           time.Duration
+	successRate        uint64
+	consecutiveSuccess int64
+}
+
+type OpenCondition struct {
+	timeInterval        time.Duration
+	failuresRate        uint64
+	consecutiveFailures int64
+}
+
+type CBOptions struct {
+	clock clock.Clock
+
+	openConditions  OpenCondition
+	closeConditions CloseCondition
+
+	openTime        time.Duration
+	ignorableErrors []error
+}
+
+var defaultCBOptions = CBOptions{
+	openTime:        time.Duration(60),
+	ignorableErrors: []error{},
+
+	clock: clock.New(),
+
+	openConditions: OpenCondition{
+		timeInterval:        time.Duration(60),
+		failuresRate:        30,
+		consecutiveFailures: 0,
+	},
+
+	closeConditions: CloseCondition{
+		duration:           time.Duration(60),
+		successRate:        90,
+		consecutiveSuccess: 5,
+	},
+}
+
 type CircuitBreaker struct {
 	mx sync.Mutex
 
-	state breakerState
-
+	state             breakerState
 	onChangeStateHook func(from breakerState, to breakerState)
 
-	counter           *Counter
-	failureRateToOpen uint64
+	counter *Counter
+
+	CBOptions
 }
 
-func NewCircuitBreaker(failureRate uint64) *CircuitBreaker {
-	return &CircuitBreaker{
-		state:             breakerClose,
-		counter:           &Counter{},
-		failureRateToOpen: failureRate,
+func NewCircuitBreaker() *CircuitBreaker {
+	cb := &CircuitBreaker{
+		counter:   &Counter{},
+		CBOptions: defaultCBOptions,
 	}
+	cb.SetState(&closedBreakerState{})
+	return cb
 }
 
-type BreakerHandleFn func() (any, error)
-
-func (cb *CircuitBreaker) Handle(ctx context.Context, fn BreakerHandleFn) error {
-
+func (cb *CircuitBreaker) Configure(conf func(options *CBOptions)) {
+	conf(&cb.CBOptions)
 }
 
-func (cb *CircuitBreaker) Done() error {
-
+func (cb *CircuitBreaker) State() BreakerStateName {
+	return cb.state.State()
 }
 
-func (cb *CircuitBreaker) setState(state breakerState) {
+func (cb *CircuitBreaker) Success() {
+	cb.mx.Lock()
+	defer cb.mx.Unlock()
+	cb.counter.incSuccess()
+	cb.state.onSuccess(cb)
+}
+
+func (cb *CircuitBreaker) Failure() {
+	cb.mx.Lock()
+	defer cb.mx.Unlock()
+	cb.counter.incFailure()
+	cb.state.onFailure(cb)
+}
+
+func (cb *CircuitBreaker) Ready() bool {
+	cb.mx.Lock()
+	defer cb.mx.Unlock()
+	return cb.state.ready()
+}
+
+func (cb *CircuitBreaker) Do(ctx context.Context, fn BreakerHandleFn) (any, error) {
+	if !cb.Ready() {
+		return nil, ErrBreakerOpen
+	}
+
+	res, err := fn()
+	if err == nil {
+		cb.Success()
+	}
+
+	if lib.Contains(cb.ignorableErrors, err) {
+		cb.Success()
+	}
+
+	if ctxErr := ctx.Err(); ctxErr != nil {
+		cb.Failure()
+	}
+
+	cb.Failure()
+
+	return res, err
+}
+
+func (cb *CircuitBreaker) SetState(state breakerState) {
 	cb.mx.Lock()
 	defer cb.mx.Unlock()
 
@@ -101,4 +187,50 @@ func (cb *CircuitBreaker) handleOnStateChange(from breakerState, to breakerState
 	}
 
 	cb.onChangeStateHook(from, to)
+}
+
+type BreakerHandleFn func() (any, error)
+
+func (cb *CircuitBreaker) checkOpen() bool {
+	return cb.checkFRateCondition()
+}
+
+func (cb *CircuitBreaker) checkClose() bool {
+	return cb.checkSRateCondition()
+}
+
+func (cb *CircuitBreaker) checkFRateCondition() bool {
+	counterTotal := cb.counter.failure + cb.counter.success
+
+	if cb.openConditions.failuresRate <= 0 {
+		return false
+	}
+
+	return cb.counter.failure/counterTotal >= cb.openConditions.failuresRate/100
+}
+
+func (cb *CircuitBreaker) checkSRateCondition() bool {
+	counterTotal := cb.counter.failure + cb.counter.success
+
+	if cb.closeConditions.successRate <= 0 {
+		return false
+	}
+
+	return cb.counter.success/counterTotal >= cb.closeConditions.successRate/100
+}
+
+func (cb *CircuitBreaker) checkConsecutiveFailuresCondition() bool {
+	if cb.openConditions.consecutiveFailures <= 0 {
+		return false
+	}
+
+	return cb.counter.consecutiveSuccess >= cb.counter.consecutiveFailure
+}
+
+func (cb *CircuitBreaker) checkConsecutiveSuccessCondition() bool {
+	if cb.closeConditions.consecutiveSuccess <= 0 {
+		return false
+	}
+
+	return cb.counter.consecutiveSuccess >= cb.counter.consecutiveSuccess
 }
